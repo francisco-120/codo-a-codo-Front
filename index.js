a0dcc86c4b3d2381d71eca085cca7b5c24283a77
francisco-120 <valdiviezofrancisco02@gmail.com>
2024-05-20 10:14:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function mostrarNav(){
+    const sidebar = document.querySelector('.barralateral')
+    sidebar.style.display= 'flex'
+}
+function ocultarNav(){
+    const sidebar = document.querySelector('.barralateral')
+    sidebar.style.display= 'none'
+}
